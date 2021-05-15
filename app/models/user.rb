0a2a7a3c5c8135d0598ef5109e6625021329be9a
sho20210126 <shohei210321@gmail.com>
2021-05-15 20:26:41 +0900

    ユーザ管理機能#4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,21 @@ class User < ApplicationRecord
          has_many :purchase_managements
          validates :email, uniqueness: { case_sensitive: true }
 
-         with_options presence: true do
+        with_options presence: true do
           validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥a-zA-Z0-9]+\z/}
           validates :email, format: { with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-          validates :password,format: { with: /[a-z\d]{6,}/i}
-          validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
-          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
-          validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-          validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/i}
+          validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+         
+            with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/} do
+              validates :family_name
+              validates :first_name
+            end
+
+            with_options format: { with: /\A[ァ-ヶー－]+\z/i} do
+              validates :family_name_kana
+              validates :first_name_kana
+            end
+
           validates :birth_day, format: { with: /\A[0-9]/}
         end
 end
