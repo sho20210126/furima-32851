dfeeff8ca6d4ba174f8075ada20c6bbe7ef1eb39
sho20210126 <shohei210321@gmail.com>
2021-06-04 16:54:01 +0900

    商品削除機能#3

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -50,7 +50,7 @@ class ProductsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @product.user_id
+    unless current_user.id == @product.user_id
       redirect_to root_path
     end
   end
